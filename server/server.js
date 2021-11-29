ecd0768961ad30a56663a22be1e3e2d2a414462a
ChristopheRibault <c.ribault.dev@gmail.com>
2021-11-29 15:41:24 +0100

    chore: setup config

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@cluster0.t6hf6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
